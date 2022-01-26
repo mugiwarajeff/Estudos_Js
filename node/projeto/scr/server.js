d7abff62d5bfd6c9a8742168cfff9efabfcb9b30
Jeff <jeffersoncerqueira75@gmail.com>
2022-01-25 22:50:38 -0300

    enviando finalização de projeto crud com express

--- node/projeto/scr/server.js
+++ node/projeto/scr/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const bancoDeDados = require("./bancoDeDados.js");
+const bodyParser = require("body-parser");
 
 const porta = 3000;
 
 const app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 
 app.get("/produtos", (req, res, next) => {
@@ -19,6 +23,21 @@ app.post("/produtos", (req, res, next) => {
         nome: req.body.nome,
         preco: req.body.preco
     })
+    res.send(produto);
+})
+
+app.put("/produtos/:id", (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        id: req.params.id,
+        nome: req.body.nome,
+        preco: req.body.preco
+    })
+    res.send(produto);
+})
+
+app.delete("/produtos/:id", (req, res, next) => {
+    const produto = bancoDeDados.deleterProdutos(req.params.id);
+    res.send(produto);
 })
 
 app.listen(porta, () => {
