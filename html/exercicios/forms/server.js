f7b65129b945a1b07a0a09485b731e79721c01f8
jefferson <jeffersoncerqueira75@gmail.com>
2022-02-18 13:27:49 -0300

    revisando estudos

--- html/exercicios/forms/server.js
+++ html/exercicios/forms/server.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const porta = 3003;
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.post("/usuarios", (req, res, next) =>{
-    console.log(req.body);
-    res.send("<h1>PARABENS USUARIO INCLUIDO</h1>")
+app.post("/usuarios", (req, res, next) => {
+    console.log(req.body)
+    res.send("<h1>Parabens a pagina foi incluida</h1>")
 })
 
-app.get("/usuarios/:id", (req, res, next) =>{
+app.post("/usuarios/:id", (req, res, next) => {
     console.log(req.body);
     console.log(req.params.id);
-    res.send("<h1>PARABENS USUARIO OBTIDO</h1>")
-}) 
-
-app.listen(porta, () => {
-    console.log(`ouvindo na porta ${porta}`)
+    res.send("<h1>Parabens a pagina foi alterada</h1>")
 })
+app.listen(porta, () => {
+    console.log(`Ouvindo na porta ${porta}`)
+});
