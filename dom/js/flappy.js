5bcf59083812548bc23d4eedf1dbc60e25435ce0
jefferson <jeffersoncerqueira75@gmail.com>
2022-04-18 22:54:45 -0300

    criando funcionalidade de movimentação da barreira

--- dom/js/flappy.js
+++ dom/js/flappy.js
@@ -15,19 +15,28 @@ function barreira(elementType, elementClass){
 function parBarreiras(elementType, elementClass, initialPosition){
     const barreiraTop = new barreira("div", "barreira-top-container");
     const barreiraBottom = new barreira("div", "barreira-bottom-container");
+    let currentPosition = initialPosition;
 
     const barreirasContainer = document.createElement(elementType);
     barreirasContainer.classList.add(elementClass);
-    barreirasContainer.style.left = initialPosition;
-    
+    //barreirasContainer.style.left = currentPosition;
 
     barreirasContainer.appendChild(barreiraTop);
     barreirasContainer.appendChild(barreiraBottom);
+
+    function moveParBarreira(){
+        console.log(currentPosition)
+        currentPosition -= 1;
+        barreirasContainer.style.left = `${currentPosition}px`;
+    }
+    setInterval(() => {
+        moveParBarreira()
+    }, 1);
     return barreirasContainer;
 }
 
-const b1 = new parBarreiras("div", "barreiras-container", "0px");
-const b2 = new parBarreiras("div", "barreiras-container", "600px");
+const b1 = new parBarreiras("div", "barreiras-container", 1500);
+const b2 = new parBarreiras("div", "barreiras-container", 1100);
 document.querySelector("[wm-flappy]").appendChild(b1);
 document.querySelector("[wm-flappy]").appendChild(b2);
 
