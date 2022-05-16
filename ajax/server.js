fd494d05f41a0b6f46b153d00b39e4abc0e3a8b9
Jefferson <jeffersoncerqueira75@gmail.com>
2022-05-16 11:41:51 -0300

    finalizando revisão de estudos da primeira parte de ajax

--- ajax/server.js
+++ ajax/server.js
@@ -1,8 +1,19 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const multer = require("multer");
 const door = 8080;
 
 const app = express();
+const storage = multer.diskStorage({
+    destination: function(req, file, callback){
+        callback(null, "./upload")
+    },
+    filename: function(req, file, callback){
+        callback(null, `${Date.now}_${file.originalname}`);
+    }
+})
+
+const upload = multer({storage: storage}).single("file");
 
 app.use(express.static("."));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -13,6 +24,14 @@ app.get("/teste", (req, res) => {
     res.send("Okay!");
 })
 
+app.post("/upload", (req, res) => {
+    upload(req, res, err => {
+        if (err){
+            return res.send("ocorreu um erro!");
+        }
+        res.send("Carregamento concluido!")
+    })
+})
 app.listen(door, () => {
     console.log("rodando...")
 });
