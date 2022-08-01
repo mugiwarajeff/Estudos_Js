f2e50d31c14fa58d128d6a7cc3e395cc560fd588
Jeff <jeffersoncerqueira75@gmail.com>
2022-07-31 22:02:28 -0300

    finalizando projeto calculadora

--- react/calculator/src/components/Button/Button.jsx
+++ react/calculator/src/components/Button/Button.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import "./Button.css"
 
-export default (props) => (
-        <button className="button">{props.label}</button>
-)
+export default (props) => {
+        let classes = ""
+        classes += props.double ? "double " : "";
+        classes += props.triple ? "triple " : "";
+        classes += props.operation ? "operation " : "";
+        
+        return (
+                <button 
+                onClick={e => props.click(props.label)}
+                className={`${classes}`}>{props.label}</button>
+        )    
+}
+        
